1578c619eb222426ed900641a05008d1981b0ed1
eriksegecs <eriksegecs@yahoo.com.br>
2023-04-08 12:37:13 -0300

    add git ignore

--- auth_steam.py
+++
@@ -1,6 +0,0 @@
-
-from pysteamsignin.steamsignin import SteamSignIn
-
-steamLogin = SteamSignIn()
-
-encodedData = steamLogin.ConstructURL('https://steamcommunity.com/openid/login?openid.ns=http://specs.openid.net/auth/2.0&openid.mode=checkid_setup&openid.return_to=https://evgouxjl7ceh43x3yclwzpipfi0ccytl.lambda-url.us-east-1.on.aws/&openid.claimed_id=http://specs.openid.net/auth/2.0/identifier_select&openid.identity=http://specs.openid.net/auth/2.0/identifier_select')
