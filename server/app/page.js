a57a82eea7ffd604d686ddc6427b2c172b9168ff
eriksegecs <eriksegecs@yahoo.com.br>
2023-06-05 23:51:37 -0300

    Updates

--- server/app/page.js
+++ server/app/page.js
@@ -328,13 +328,13 @@ __webpack_require__.r(__webpack_exports__);
 
 /***/ }),
 
-/***/ 5548:
+/***/ 4706:
 /***/ ((__unused_webpack_module, __unused_webpack_exports, __webpack_require__) => {
 
 Promise.resolve(/* import() eager */).then(__webpack_require__.t.bind(__webpack_require__, 125, 23));
 Promise.resolve(/* import() eager */).then(__webpack_require__.t.bind(__webpack_require__, 6249, 23));
-Promise.resolve(/* import() eager */).then(__webpack_require__.t.bind(__webpack_require__, 1522, 23));
 Promise.resolve(/* import() eager */).then(__webpack_require__.t.bind(__webpack_require__, 7844, 23));
+Promise.resolve(/* import() eager */).then(__webpack_require__.t.bind(__webpack_require__, 1522, 23));
 Promise.resolve(/* import() eager */).then(__webpack_require__.t.bind(__webpack_require__, 3100, 23))
 
 /***/ }),
