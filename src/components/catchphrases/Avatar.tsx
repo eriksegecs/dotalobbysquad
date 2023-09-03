0ca347ecf173b1ff7b7f73023fc7c7621db44b40
christyansegecs <christyansegecs@hotmail.com>
2023-09-03 18:54:00 -0300

    fix pipeline in Avatar class

--- src/components/catchphrases/Avatar.tsx
+++ src/components/catchphrases/Avatar.tsx
@@ -7,8 +7,8 @@ interface AvatarProps extends ImgHTMLAttributes<HTMLImageElement> {
 }
 
 
-export function Avatar({ hasBorder = true, src, ...props } : AvatarProps) {
+export function Avatar({ hasBorder = true, ...props } : AvatarProps) {
     return (
-        <Image src={src} alt='' className={'w-14 h-14 rounded-md border-4 border-[#202024] ring-2 ring-yellow-400'}></Image>
+        <Image src={''} alt='' className={'w-14 h-14 rounded-md border-4 border-[#202024] ring-2 ring-yellow-400'}></Image>
     )
 }
