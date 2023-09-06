79f40242d4e90337b7be9e98f6005d1bd784e1d0
christyansegecs <christyansegecs@hotmail.com>
2023-09-06 16:39:29 -0300

    test next config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
 
+const isProd = process.env.NODE_ENV === "production"
+const prefix = isProd ? "/dotalobbysquad" : ""
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
 }
 
-module.exports = nextConfig
+module.exports = {
+  basePath: prefix,
+  assetPrefix: prefix,
+}
