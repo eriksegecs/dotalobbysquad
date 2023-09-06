e71cd2640958d4f751c529e6ef89a14d791f649c
christyansegecs <christyansegecs@hotmail.com>
2023-09-06 16:44:57 -0300

    fix next config base url and prefix url for production

--- next.config.js
+++ next.config.js
@@ -1,13 +1,12 @@
 
-const isProd = process.env.NODE_ENV === "production"
-const prefix = isProd ? "/dotalobbysquad" : ""
+const isProd = process.env.NODE_ENV === "production";
+const prefix = isProd ? "/dotalobbysquad" : "";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-}
-
-module.exports = {
   basePath: prefix,
   assetPrefix: prefix,
 }
+
+module.exports = nextConfig
