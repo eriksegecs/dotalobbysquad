5b270a8959dfa8db887b5a4833ee5fe1c72eb8bf
christyansegecs <christyansegecs@hotmail.com>
2023-09-06 18:06:59 -0300

    test

--- next.config.js
+++ next.config.js
@@ -1,19 +1,9 @@
 
-const isGithubActions = process.env.GITHUB_ACTIONS || false
-
-let assetPrefix = ''
-let basePath = '/'
-
-if (isGithubActions) {
-  // trim off `<owner>/`
-  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
-
-  assetPrefix = `/${repo}/`
-  basePath = `/${repo}`
-}
-
-module.exports = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   output: 'export',
-  assetPrefix: assetPrefix,
-  basePath: basePath,
+  basePath: '/',
+
 }
+
+module.exports = nextConfig
